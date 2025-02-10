2a7949eb4502b23ab8760e9a3fc2ca0fdba9117f
nitzan <nitzan.h.2000@gmail.com>
2025-02-10 21:07:12 +0200

    add navigation logic

--- app/src/main/java/com/example/foodguard/fragments/EditProfileFragment.kt
+++ app/src/main/java/com/example/foodguard/fragments/EditProfileFragment.kt
@@ -1,21 +1,20 @@
 package com.example.foodguard.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
 import com.example.foodguard.R
 
-/**
- * A fragment representing a list of Items.
- */
-class PostDetailsFragment : Fragment() {
+class EditProfileFragment  : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_post_details, container, false)
+        return inflater.inflate(R.layout.fragment_edit_profile, container, false)
     }
 }
