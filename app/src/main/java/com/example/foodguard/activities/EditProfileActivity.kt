46690a13b898444ed8b970c8f60fd2b1c0b487e5
nitzan <nitzan.h.2000@gmail.com>
2025-02-10 21:01:20 +0200

    add navigation bar

--- app/src/main/java/com/example/foodguard/activities/EditProfileActivity.kt
+++ app/src/main/java/com/example/foodguard/activities/EditProfileActivity.kt
@@ -7,11 +7,11 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.foodguard.R
 
-class EditPostActivity : AppCompatActivity() {
+class EditProfileActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_edit_post)
+        setContentView(R.layout.activity_edit_profile)
 //        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
 //            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
 //            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
